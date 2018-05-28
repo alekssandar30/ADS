c440e7d29af8ad4a3deddfba3b73e61478f6946c
Aleksandars <33751361+alekssandar30@users.noreply.github.com>
2018-05-28 16:31:18 +0200

    Add files via upload

---
+++ Redudantni_klijent/KonfiguracijaServera.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class KonfiguracijaServera
+    {
+        private  EStanjeServera stanjeServera;
+        public  EStanjeServera StanjeServera
+        {
+            get => stanjeServera;
+            set => stanjeServera = value;
+        }
+        public KonfiguracijaServera()
+        {
+            stanjeServera
+            = Properties.Settings.Default.StanjeServera;
+            Console.WriteLine("Stanje servisa je: "
+            + StanjeServera.ToString());
+        }
+    }
+}
