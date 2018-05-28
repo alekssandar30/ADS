9e3aeef310a0502ef034dcb90a27c57bbdadba2a
Aleksandars <33751361+alekssandar30@users.noreply.github.com>
2018-05-28 16:33:31 +0200

    Add files via upload

---
+++ Redudantni_klijent/Server/ObradaStanja.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ObradaStanja : IStanjeServisa
+    {
+        public static KonfiguracijaServera konfiguracija
+        = new KonfiguracijaServera();
+
+       
+
+        public void AzuriranjeStanja(EStanjeServera stanje)
+        {
+            konfiguracija.StanjeServera = stanje;
+            Console.WriteLine(stanje);
+        }
+        public EStanjeServera ProveraStanja()
+        {
+            return ObradaStanja.konfiguracija.StanjeServera;
+        }
+    }
+
+}
