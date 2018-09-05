ab306edef6b59e6d28b826fb44831527aeb01139
Aleksandar <33751361+alekssandar30@users.noreply.github.com>
2018-09-05 18:28:27 +0200

    Add files via upload

---
+++ Autentifikacija/Common/IPersonService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+        [ServiceContract]
+        public interface IPersonService
+        {
+            [OperationContract]
+            [FaultContract(typeof(SecurityException))]
+            void AddPerson(Person person, string username);
+
+            [OperationContract]
+            [FaultContract(typeof(DataException))]
+            [FaultContract(typeof(SecurityException))]
+            void RemovePerson(long jmbg, string username);
+
+            [OperationContract]
+            [FaultContract(typeof(SecurityException))]
+            List<Person> GetAllPersons(string username, DateTime timeRequest);
+
+            [OperationContract]
+            [FaultContract(typeof(SecurityException))]
+            void AddAllPersons(List<Person> persons, string username);        
+    }
+}
---
+++ Autentifikacija/Common/ISystemUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface ISystemUserService
+    {
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        string Authenticate(string user, string pass);
+    }
+}
