42e96f056c9b823a24b5f1325865ae3843bb7d7a
Aleksandar <33751361+alekssandar30@users.noreply.github.com>
2018-09-05 18:23:11 +0200

    Add files via upload

---
+++ Replikacija/Common/DataException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class DataException
+    {
+        private string _message;
+
+        [DataMember]
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+
+            set
+            {
+                _message = value;
+            }
+        }
+    }
+}
